e855418785c34918017f4f5cc3ad1375b4f5012f
Amey <ameygawade11@gmail.com>
2025-08-02 12:27:47 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/test",(req, res) => {
+    res.send("Hello from the server!")
+})
+
+app.use("/hello",(req, res) => {
+    res.send("Hello hello no hello!")
+})
+
+
+app.listen(7777, ()=> {
+    console.log("Server is running successfully running on port 3000...");
+});

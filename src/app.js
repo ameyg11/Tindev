c550c94dffaadb3fe6a7a5f14acb499bd8fd73bc
Amey <ameygawade11@gmail.com>
2025-08-17 21:01:36 +0530

    created API for deleting data fromthe database using _id

--- src/app.js
+++ src/app.js
@@ -59,6 +59,17 @@ app.get("/feed", async (req, res) => {
 
 })
 
+app.delete("/user", async (req, res) => {
+    const userID = req.body.userId;
+
+    try{
+        await User.findByIdAndDelete(userID);
+        res.send("User deleted successfully")
+    }catch(err){
+        res.status(400).send("Something went wrong");
+    }
+})
+
 connectDB()
     .then(() => {
         console.log("database successfully connected");

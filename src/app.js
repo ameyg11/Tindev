c7d54e4f6fb049daa8a22ed8d61dd8c50ed85f65
Amey <ameygawade11@gmail.com>
2025-08-22 23:30:34 +0530

    refactor user routes for improved readability and added validation for allowed updates

--- src/app.js
+++ src/app.js
@@ -1,98 +1,103 @@
 const connectDB = require("./config/database");
 const express = require("express");
 const app = express();
-const User = require("./models/user")
+const User = require("./models/user");
 
-app.use(express.json())
-
+app.use(express.json());
 
 // posting data to database
 app.post("/signup", async (req, res) => {
-    // console.log(req.body);
-    
-    {/*const userObj = {
+  // console.log(req.body);
+
+  {
+    /*const userObj = {
         firstName: "Virat",
         lastName: "Kohli",
         emailId: "viraat@gmail.com",
         password: "98765321"
-    }*/}
+    }*/
+  }
 
-    // Creating a new instance of a User model
-    const user = new User(req.body);
+  // Creating a new instance of a User model
+  const user = new User(req.body);
 
-    try {
-        // throw new Error("1234567")
-        console.log(req.body)
-        await user.save();
-        res.send("new user logged In")
-    }catch(err) {
-        res.send("there was some error",err)
-    }
+  try {
+    // throw new Error("1234567")
+    console.log(req.body);
+    await user.save();
+    res.send("new user logged In");
+  } catch (err) {
+    res.send("there was some error", err);
+  }
+});
 
-})
-
-// getting data from database 
+// getting data from database
 app.get("/user", async (req, res) => {
+  const userMail = req.body.emailID;
 
-    const userMail = req.body.emailID;
-
-    try{
-        const user = await User.find({ emailId: userMail})
-        res.send(user);
-    }catch(err){
-        res.send("Something Went Wrong")
-    }
-
-})
+  try {
+    const user = await User.find({ emailId: userMail });
+    res.send(user);
+  } catch (err) {
+    res.send("Something Went Wrong");
+  }
+});
 
 app.get("/feed", async (req, res) => {
+  // const userMail = req.body.emailID;
 
-    // const userMail = req.body.emailID;
-
-    try{
-        const user = await User.find({})
-        res.send(user);
-        // console.log(user)
-    }catch(err){
-        res.send("Something Went Wrong")
-    }
-
-})
+  try {
+    const user = await User.find({});
+    res.send(user);
+    // console.log(user)
+  } catch (err) {
+    res.send("Something Went Wrong");
+  }
+});
 
 app.delete("/user", async (req, res) => {
-    const userID = req.body.userId;
+  const userID = req.body.userId;
 
-    try{
-        await User.findByIdAndDelete(userID);
-        res.send("User deleted successfully")
-    }catch(err){
-        res.status(400).send("Something went wrong");
+  try {
+    await User.findByIdAndDelete(userID);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
+  try {
+    const ALLOWED_UPDATES = ["photoUrl", "age", "skills"];
+    const isUpdatedAllowed = Object.keys(data).every((k) => {
+        return ALLOWED_UPDATES.includes(k);
+    });
+
+    if (!isUpdatedAllowed) {
+      throw new Error("Updates of this field are not allowed");
     }
-})
 
-app.patch("/user", async(req, res) => {
-    const userId = req.body.userId;
-    const data = req.body;
-
-    try{
-        const user = await User.findByIdAndUpdate(userId, data, {
-            returnDocument: 'after',
-            runValidators: true,
-    })
-        res.send("User updated successfully")
-        console.log(user);
-    }catch(err){
-        res.status(400).send("Something went wrong" + err.message)
-    }
-})
+    const user = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send("User updated successfully");
+    console.log(user);
+  } catch (err) {
+    res.status(400).send("Something went wrong: " + err.message);
+  }
+});
 
 connectDB()
-    .then(() => {
-        console.log("database successfully connected");
-        app.listen(7777, ()=> {
-            console.log("Server is running successfully running on port 7777...");
-        });
-    })
-    .catch((err) =>{
-        console.log("Error occured",err);
-    })
+  .then(() => {
+    console.log("database successfully connected");
+    app.listen(7777, () => {
+      console.log("Server is running successfully running on port 7777...");
+    });
+  })
+  .catch((err) => {
+    console.log("Error occured", err);
+  });

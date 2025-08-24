74167c92f0fab04059bcdd26a0666c276fd35538
Amey <ameygawade11@gmail.com>
2025-08-24 10:12:01 +0530

    add dotenv for environment variable management and update database connection

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const connectDB = require("./config/database");
 const express = require("express");
 const app = express();
--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async() => {
     await 
     mongoose.connect(
-        "mongodb+srv://ameyg11:vJ4RlF1HCIJWj0fT@namastedev.vj7ydmu.mongodb.net/?retryWrites=true&w=majority&appName=NamasteDev/devTinder"
+        process.env.MONGO
     )};
 
 

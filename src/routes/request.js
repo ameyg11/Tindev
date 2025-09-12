b1e01c61f502686922eeb8acadabfb85d9820de9
Amey <ameygawade11@gmail.com>
2025-09-12 22:10:18 +0530

    added the request review API to accept or reject the connection request

--- src/routes/request.js
+++ src/routes/request.js
@@ -58,6 +58,40 @@ requestRouter.post(
     }catch(err){
       res.status(400).send(""+err)
     }
+});
+
+requestRouter.post("/request/review/:status/:requestId", userAuth, async(req, res) => {
+
+  try{
+
+    const loggedInUser = req.user;
+    const { status, requestId } = req.params;
+  
+    const allowedStatus = ["accepted", "rejected"];
+    if(!allowedStatus.includes(status)){
+      return res.status(400).json({ message: "Invalid request status!"})
+    }
+  
+    const connectionRequest = await ConnectionRequest.findOne({
+      _id: requestId,
+      toUserId: loggedInUser._id,
+      status: "interested",
+    });
+    if (!connectionRequest) {
+      return res.status(404).json({ message: "Connection request not found"})
+    }  
+  
+    connectionRequest.status = status;
+  
+    const data = await connectionRequest.save();
+
+    res.json("Connection request "+status , data)
+
+  }catch(err) {
+    res.send("ERROR : " + err);
+  }
+
 })
+
 
 module.exports = requestRouter;
